023ae9352a9c0bf14505d7ff215830fb084611dc
Your Name <you@example.com>
2024-06-21 17:04:17 +0530

    added Overview.js and JSXBasics.js

---
+++ src/JSXBasics.js
@@ -0,0 +1,14 @@
+import React from "react";
+function JSXBasics(){
+    const name='Mahesh';
+
+    return(
+        <div>
+            <h2>JSX Basics</h2>
+            <p>This is a simple JSX example.</p>
+            <h3>hello, {name} !</h3>
+        </div>
+    );
+}
+
+export default JSXBasics;
---
+++ src/Overview.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Overview(){
+    return(
+        <div>
+            <h1>What is React?</h1>
+            <p>React is a Javascript Library for building user interface</p>
+        </div>
+    );
+}
+
+export default Overview;
